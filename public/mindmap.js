1d75a7790b3113245ee087416696843b1d742083
minihate <fypinou@gmail.com>
2015-12-30 13:13:55 +0100

    Fix for Firefox

--- public/mindmap.js
+++ public/mindmap.js
@@ -33,6 +33,27 @@ context.textBaseline = 'middle';
 context.imageSmoothingEnabled = true;
 document.body.appendChild(canvas);
 
+function drawEllipse(ctx, x, y, w, h) {
+  var kappa = .5522848,
+      ox = (w / 2) * kappa, // control point offset horizontal
+      oy = (h / 2) * kappa, // control point offset vertical
+      xe = x + w,           // x-end
+      ye = y + h,           // y-end
+      xm = x + w / 2,       // x-middle
+      ym = y + h / 2;       // y-middle
+  ctx.moveTo(x, ym);
+  ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+  ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+  ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+  ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+}
+
+if (!context.ellipse) {
+    context.ellipse = function(cx, cy, w, h) {
+        drawEllipse(this, cx - w, cy - h, w * 2, h * 2);
+    };
+}
+
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
